fb9f80f889376d4f77b3a715d806fa143f9f03d1
salvation27 <nikolayzalipykin@gmail.com>
2022-03-15 22:54:27 +0200

    firebase avtorization toolk4

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: 'AIzaSyAKO2Pe5ZDNHuIaZeCE93QCCegb35XgRns',
+  authDomain: 'auth-example-6a694.firebaseapp.com',
+  projectId: 'auth-example-6a694',
+  storageBucket: 'auth-example-6a694.appspot.com',
+  messagingSenderId: '4498389263',
+  appId: '1:4498389263:web:40f90e9fb1ee5b09df667a',
 };
 
 const app = initializeApp(firebaseConfig);
